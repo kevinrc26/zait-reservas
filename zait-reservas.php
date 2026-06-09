2f10dafebe9980c310c66954a30c9b4ebbfc067a
Kevin Robles <104695657+kevinrc26@users.noreply.github.com>
2026-06-08 20:19:31 -0500

    Update zait-reservas.php

--- zait-reservas.php
+++ zait-reservas.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * Plugin Name: Zait - Redirección de Reservas
+ * Plugin Name: Zait - Redirección de Reservas (Amelia Free)
  * Version: 1.0.0
- * Description: Componente CI/CD 2: Redirige al cliente automáticamente a la plataforma de agendamiento tras un pago exitoso.
+ * Description: Componente CI/CD 2: Redirige automáticamente al paciente tras agendar con éxito una cita en el formulario de Amelia.
  * Author: Tu Nombre
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
+if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-add_filter( 'woocommerce_get_return_url', 'zait_redireccion_post_pago', 10, 2 );
-function zait_redireccion_post_pago( $return_url, $order ) {
-    // URL del botón "Reservar Cita" del Spa de Zait Sebastián
-    $url_sistema_citas = 'https://fenixestrategiadigital.com'; 
-    
-    if ( $order ) {
-        return $url_sistema_citas;
-    }
-    return $return_url;
+add_action( 'wp_footer', 'zait_redireccion_amelia_js' );
+function zait_redireccion_amelia_js() {
+    ?>
+    <script type="text/javascript">
+    window. AmeliaActions = window.AmeliaActions || {};
+    window.AmeliaActions.onBookingSuccessful = function (obj) {
+        // REEMPLAZA esta URL con la página a donde quieres enviar al paciente al terminar su reserva
+        window.location.href = "https://fenixestrategiadigital.com";
+    };
+    </script>
+    <?php
 }
